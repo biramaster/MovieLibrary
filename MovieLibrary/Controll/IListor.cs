9b7c7479c04a4ccc87f4c5e69edbb1659ba209b3
biramaster <biramaster001@gmail.com>
2015-01-26 12:14:30 +0100

    Changed IListor

---
+++ MovieLibrary/Controll/IListor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Controll
+{
+	interface IListor<T>
+	{
+		void Add(T item);
+		void Remove(T item);
+		T Get(int item);
+		int Count();
+		T Find(string strFind);
+	}
+}
---
+++ MovieLibrary/Controll/MemberList.cs
@@ -0,0 +1,37 @@
+﻿using LibrarySystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Controll
+{
+	class MemberList : IListor<Member>
+	{
+		public void Add(Member item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(Member item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Member Get(int item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Member Find(string strFind)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
