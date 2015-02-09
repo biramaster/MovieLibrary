81a262cadc1332fc6780ba6417dfe3eaa42889e9
biramaster <andre_rubira@hotmail.com>
2015-02-09 15:51:42 +0100

    Implemented addMovies

--- MovieLibrary/View/AddMember.Designer.cs
+++ MovieLibrary/View/AddMember.Designer.cs
@@ -43,7 +43,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.listView4 = new System.Windows.Forms.ListView();
             this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblPersonal
@@ -173,24 +172,14 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(109, 23);
             this.button3.TabIndex = 14;
-            this.button3.Text = "Add Movie Copy";
+            this.button3.Text = "Add Loan";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(419, 553);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 23);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "Add Loan";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // AddMember
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 636);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.listView4);
@@ -229,7 +218,6 @@
 		private System.Windows.Forms.ListView listView3;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.ListView listView4;
-		private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- MovieLibrary/View/AddMovie.cs
+++ MovieLibrary/View/AddMovie.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibrarySystem;
+using MovieLibrary.Controll;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,61 @@ namespace MovieLibrary.View
 {
     public partial class AddMovie : Form
     {
+        private MovieList movieList;
         public AddMovie()
         {
             InitializeComponent();
+            try
+            {
+                movieList = ServiceProvider.GetMovieService();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            movieList.Updated += new EventHandler(movieService_Update);
+            initListView();
+        }
+
+        public void movieService_Update(object sender, EventArgs e)
+        {
+            updateListView();
+        }
+
+        private void initListView()
+        {
+            //         lvwMovie.View = View.Details;
+            lvwMovie.FullRowSelect = true;
+            lvwMovie.GridLines = true;
+            // Add Columns
+            lvwMovie.Columns.Add("ID", -2, HorizontalAlignment.Left);
+            lvwMovie.Columns.Add("Title", -2, HorizontalAlignment.Left);
+            lvwMovie.Columns.Add("Director", -2, HorizontalAlignment.Left);
+
+            updateListView();
+
+        }
+
+        private void updateListView()
+        {
+            lvwMovie.Items.Clear();
+            string[] columns = new string[3];
+            ListViewItem item;
+            for (int i = 0; i < movieList.Count(); i++)
+            {
+                columns[0] = movieList.Get(i).getId().ToString();
+                columns[1] = movieList.Get(i).getTitle();
+                columns[2] = movieList.Get(i).getDirector();
+                item = new ListViewItem(columns);
+                lvwMovie.Items.Add(item);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
